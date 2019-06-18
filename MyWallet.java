eb9bf7affaf92a412b1fe200320b0608102e16ff
Tan24CapWorkspace <51941066+Tan24CapWorkspace@users.noreply.github.com>
2019-06-18 04:24:55 +0000

    Add files via upload

---
+++ MyWallet.java
@@ -0,0 +1,19 @@
+package com.cg.pl;
+import com.cg.bean.*;
+public class MyWallet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Account ob=new Account(100,11111111,"King",10000.00);
+		
+		System.out.println(ob);
+		double b1=ob.withdraw(2000.00);
+		System.out.println(b1);
+		SavingAccount ob1=new SavingAccount(101,22222222,"Raja",25000.00);
+		ob1.setInterest();
+		System.out.println(ob1);
+		b1=ob1.withdraw(24500);
+		System.out.println(b1);
+	}
+
+}
